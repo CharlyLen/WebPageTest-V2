92c36331a81280e7c8e083720d5e1d12d52af799
CharlyLen <carlospaniaguafer@gmail.com>
2021-12-06 10:39:49 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using WebPageTest.Models;
+
+namespace WebPageTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GeoExample()
+        {
+            JSONToViewModel model = new JSONToViewModel();
+            GeoHelper geoHelper = new GeoHelper();
+            var result = await geoHelper.GetGeoInfo();
+            model = JsonConvert.DeserializeObject<JSONToViewModel>(result);
+            //TempData["GeoCode"] = result;
+            return View(model);
+        }
+
+        //cuando llamamos la pagina pero con un parametro (ip) [c]
+        [HttpPost]
+        public async Task<ActionResult> GeoExample(string ip)
+        {
+            JSONToViewModel model = new JSONToViewModel();
+            GeoHelper geoHelper = new GeoHelper();
+            var result = await geoHelper.GetGeoInfo(ip);
+            model = JsonConvert.DeserializeObject<JSONToViewModel>(result);
+            //TempData["GeoCode"] = result;
+            ViewBag.mensaje = "IP obtenida";
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
